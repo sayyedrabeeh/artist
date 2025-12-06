40abb3948250da250c939fafb13cc03da5271e2a
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-12-06 18:53:41 +0530

    Fix images for Render

--- sketch_project/sketch_project/urls.py
+++ sketch_project/sketch_project/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('', home, name='home'),
     path("upload/", uploadImage, name="upload"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
